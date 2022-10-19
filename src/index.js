dce806ee4279876f10ae9ecbb63959cdac8979a9
QianlingC <chowqianlingg@gmail.com>
2022-10-20 00:13:56 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import App from "./App";
+// import AppData from "./AssignemtWk2D2/AppData";
+// import GroceryDisplay from "./AssignemtWk2D2/GroceryDisplay";
+// import GroceryClass from "./AssignemtWk2D2/GroceryClass";
+import EmployeeData from "./AssignmentWk2D3/EmployeeData";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <AppData /> */}
+    {/* <App /> */}
+    {/* <GroceryDisplay /> */}
+    {/* <GroceryClass /> */}
+    <EmployeeData />
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -14,4 +21,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
